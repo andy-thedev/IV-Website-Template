8ec72a4010e8e884627b50ec11f4319b42bc5d74
Won Tae Jung <wontae.andyjung@gmail.com>
2021-01-09 22:28:17 -0500

    Testing iframe optimization

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 new WOW().init();
 
+function init() {
+    var vidDefer = document.getElementsByTagName('iframe');
+    for (var i=0; i<vidDefer.length; i++) {
+    if(vidDefer[i].getAttribute('data-src')) {
+    vidDefer[i].setAttribute('src',vidDefer[i].getAttribute('data-src'));
+    } } }
+
+window.onload = init;
+
 $(document).ready(
     function(){
         $('.navbar-toggler').click(function(){
